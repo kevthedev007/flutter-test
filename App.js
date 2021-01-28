16c02ed142c89d8e49d8bddabb5fd7306f53cbe7
kevthedev007 <63928159+kevthedev007@users.noreply.github.com>
2021-01-28 04:57:09 +0100

    app

--- App.js
+++ App.js
@@ -12,6 +12,6 @@ app.use(express.urlencoded({extended: true}))
 
 app.use('/', routes)
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`app started on ${process.env.PORT}`)
 })
