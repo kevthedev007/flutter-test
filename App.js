d086594e0a15da4b20354bf963077c43808c169a
kevthedev007 <63928159+kevthedev007@users.noreply.github.com>
2021-01-29 13:51:24 +0100

    all good

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 
 //create express app
 const app = express()
@@ -8,17 +7,6 @@ const app = express()
 const routes = require('./router/router.js');
 
 //middleware
-// app.use((req, res, next) => {
-//     if(req.headers['content-type'] !== 'application/json' && req.body !== "object") {
-//         return res.json({
-//             "message": "Invalid JSON payload passed.",
-//             "status": "error",
-//             "data": null
-//         })
-//     };
-//     next()
-// })
-
 app.use(express.json())
 
 //routing
